2f09391e913917e1f89b6bdc4edc250db3bf1c37
admin <you@example.com>
2025-04-30 13:52:58 +0900

    9주차

--- js/js_popup.js
+++ js/js_popup.js
@@ -10,12 +10,46 @@
 // close_time2--; // 1초씩 감소
 // setTimeout(show_time, 1000); //1초마다 갱신
 // }
-// function close_window() { // 함수 정의
-// window.close(); // 윈도우 닫기
-// }        //파일로 분리해서 popup.html에 로딩해야됨
+function close_window() { // 함수 정의
+window.close(); // 윈도우 닫기
+}        //파일로 분리해서 popup.html에 로딩해야됨
+
+function setCookie(name, value, expiredays) {
+    var date = new Date();
+    date.setDate(date.getDate() + expiredays);
+    document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + ";path=/" + ";SameSite=None; Secure";
+    }
+
+function getCookie(name) {
+var cookie = document.cookie;
+console.log("쿠키를 요청합니다.");
+if (cookie != "") {
+var cookie_array = cookie.split("; ");
+for ( var index in cookie_array) {
+var cookie_name = cookie_array[index].split("=");
+if (cookie_name[0] == "popupYN") {
+return cookie_name[1];
+}
+}
+}
+return ;
+}
+
+function closePopup() {
+    if (document.getElementById('check_popup').value) {
+    setCookie("popupYN", "N", 1);
+    console.log("쿠키를 설정합니다.");
+    self.close();
+    }
+    }
+    
 
 function pop_up() {
-    window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+    var cookieCheck = getCookie("popupYN");
+    if (cookieCheck != "N")
+        {
+        window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+        }
     }
 
     
