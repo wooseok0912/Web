af7a2b7073e5e1585277771174812f046c9acd9c
admin <you@example.com>
2025-05-07 14:16:18 +0900

    10주차 실습

--- js/js_login.js
+++ js/js_login.js
@@ -1,3 +1,15 @@
+function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
+    const emailInput = document.getElementById('typeEmailX');
+    const idsave_check = document.getElementById('idSaveCheck');
+    let get_id = getCookie("id");
+    if(get_id) {
+        emailInput.value = get_id;
+        idsave_check.checked = true;
+    }
+    session_check();
+}
+    
+
 const check_xss = (input) => {
     // DOMPurify 라이브러리 로드 (CDN 사용)
     const DOMPurify = window.DOMPurify;
@@ -13,20 +25,43 @@ const check_xss = (input) => {
     return sanitizedInput;
     };
     
+    function setCookie(name, value, expiredays) {
+        var date = new Date();
+        date.setDate(date.getDate() + expiredays);
+        document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + ";path=/" + ";SameSite=None; Secure";
+        }
+        
+    function getCookie(name) {
+    var cookie = document.cookie;
+    console.log("쿠키를 요청합니다.");
+    if (cookie != "") {
+    var cookie_array = cookie.split("; ");
+    for ( var index in cookie_array) {
+    var cookie_name = cookie_array[index].split("=");
+    if (cookie_name[0] == "id") {
+    return cookie_name[1];
+    }
+    }
+    }
+    return ;
+    }
 
-const check_input = () => {
+    const check_input = () => {
     const loginForm = document.getElementById('login_form');
     const loginBtn = document.getElementById('login_btn');
     const emailInput = document.getElementById('typeEmailX');
+    // 전역 변수 추가, 맨 위 위치
+    const idsave_check = document.getElementById('idSaveCheck');
     const passwordInput = document.getElementById('typePasswordX');
     const c = '아이디, 패스워드를 체크합니다';
     alert(c);
     const emailValue = emailInput.value.trim();
     const passwordValue = passwordInput.value.trim();
-    const sanitizedPassword = check_xss(passwordInput);
+    const sanitizedPassword = check_xss(passwordValue);
     // check_xss 함수로 비밀번호 Sanitize
-    const sanitizedEmail = check_xss(emailInput);
+    const sanitizedEmail = check_xss(emailValue);
     // check_xss 함수로 비밀번호 Sanitize
+
     if (emailValue === '') 
         {
         alert('이메일을 입력하세요.');
@@ -75,9 +110,18 @@ const check_input = () => {
         return false;
         }
         
+    if(idsave_check.checked == true) { // 아이디 체크 o
+        alert("쿠키를 저장합니다.", emailValue);
+        setCookie("id", emailValue, 1); // 1일 저장
+        alert("쿠키 값 :" + emailValue);
+        }
+    else{ // 아이디 체크 x
+        setCookie("id", emailValue.value, 0); //날짜를 0 - 쿠키 삭제
+        }    
 
     console.log('이메일:', emailValue);
     console.log('비밀번호:', passwordValue);
+    session_set(); //세션생성
     loginForm.submit();
     };
     document.getElementById("login_btn").addEventListener('click', check_input);
